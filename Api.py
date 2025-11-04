5d887369dddcc3d67bb207611beb438f9df78972
jose <joseenriquezayas2012@gmail.com>
2025-11-03 22:53:54 -0300

    Cambio en la Api

--- Api.py
+++ Api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, status
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import List
 from bson import ObjectId
-from .schemas import Usuario, UsuarioCreate, Producto, ProductoCreate 
+from Database import Usuario, UsuarioCreate, Producto, ProductoCreate 
 
 
 MONGO_URI = "mongodb://localhost:27017" 
